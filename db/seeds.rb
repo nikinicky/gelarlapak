4cb0c67df2878fbb8ee62d4055d9596c47c2c6df
Nicky Valentino <nickyv.valentino@gmail.com>
2021-07-22 23:27:44 +0700

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,35 @@
 # Seed shops
-[
-  {
-    name: 'Tokopaedi',
-    address: 'Jakarta Selatan',
-    description: '',
-    shop_type: 'bronze'
-  }
-].each do |shop|
-  Shop.find_or_create_by(shop)
+15.times do
+  Shop.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.city,
+    description: Faker::Lorem.paragraph,
+    shop_type: Shop::SHOP_TYPES.sample,
+    status: 'active'
+  )
 end
+  
 
 # Seed products
 shops = Shop.all
 
-[
-  {
-    name: "iPad Mini 5 Wi-Fi Cell 6GB 64GB",
+50.times do
+  product = Product.create!(
+    name: Faker::Commerce.product_name,
     shop_id: shops.sample.id,
-    description: 'BNIB',
-    average_rating: rand(3.0...5.0).round(2),
-    variant: [
-      {name: 'Grey', price_cents: 20000, price_currency: 'USD', stock: rand(0...10)},
-      {name: 'Gold', price_cents: 21000, price_currency: 'USD', stock: rand(0...10)},
-      {name: 'Silver', price_cents: 22000, price_currency: 'USD', stock: rand(0...10)}
-    ]
-  }
-].each do |product_params|
-  product = Product.find_or_create_by(product_params.except(:variant))
+    description: Faker::Lorem.paragraph,
+    average_rating: rand(3.0...5.0).round(1),
+    status: 'active'
+  )
 
-  product_params[:variant].each do |variant_params|
-    variant_params[:product_id] = product.id
-    ProductVariant.find_or_create_by(variant_params)
+  rand(1...5).times do
+    ProductVariant.create!(
+      product_id: product.id,
+      name: Faker::Commerce.color,
+      price_cents: (Faker::Commerce.price * 100),
+      price_currency: 'USD',
+      stock: rand(0...5),
+      status: 'active'
+    )
   end
 end
